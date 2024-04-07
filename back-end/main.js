2c0443729c1a35cacc28a1934cee309d687aebbe
salvo1992 <145794449+salvo1992@users.noreply.github.com>
2024-04-07 20:59:26 +0200

    init

--- back-end/main.js
+++ back-end/main.js
@@ -23,9 +23,35 @@ app.use('/',booksRoute);
 app.use('/',loginRoute);
 //connessione al database
 
-connectToDatabase()
 
 
 
 
-app.listen(PORT, ()=>console.log(`server connected and listening on port ${PORT}`));   
+
+
+
+
+
+//connessione al database
+
+
+
+
+mongoose.connect (process.env.MONGODB_URL);
+
+const db =mongoose.connection;
+db.on('error',console.error.bind(console,'Db connection error'));
+
+db.once('open',()=>{
+    console.log('Database successfully connected!');
+})
+
+
+
+
+
+
+
+
+
+app.listen(PORT, ()=>console.log(`server connected and listening on port ${PORT}`)); 
